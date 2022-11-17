319dbf0931bec7cb5ec60d39e96abf672d79912b
Anthony <delgadad@sunypoly.edu>
2022-11-16 23:46:06 -0500

    Moved api to seperate page and added more api calls

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ import bodyParser from "body-parser";
 
 const PORT = 3000;
 const IMAGEPATH = "./tensor_flow/Samples"
+const NOTEBOOKSPATH = "./tensor_flow"
 let SESSIONS = [];
+let KERNELS = [];
 let DIRCONTENTS = [];
 let TRAINPROGRESS = null;
+let NOTEBOOKS = null;
 
 //View engine
 app.set('view engine', 'ejs');
@@ -17,6 +20,8 @@ app.use(express.static('public'));
 app.use(express.static('tensor_flow/Samples'));
 app.use(express.urlencoded({ extended: true}));
 app.use(bodyParser.json())
+
+shell.config.silent = true;
 
 const server = app.listen(PORT, () => {
   console.log(`Express running → PORT ${server.address().port}`);
@@ -30,19 +35,24 @@ function getImageFiles(){
 
 function getAI(){
   let aihtml = shell.exec(`curl -X GET http://localhost:8080/ai `);
-  console.log(aihtml);
 }
 
 app.get('/', (req, res) => {
   getImageFiles();
   getAI();
-  res.render('index.ejs', {title: 'HOME', sessions: SESSIONS, files: DIRCONTENTS, trainProgress:TRAINPROGRESS});
+  res.render('index.ejs', {title: 'HOME', kernels: KERNELS, sessions: SESSIONS, files: DIRCONTENTS, trainProgress:TRAINPROGRESS, notebooks: NOTEBOOKS});
 });
 
 app.get('/get_session', (req, res) => {
   SESSIONS = shell.exec(`curl -X GET http://127.0.0.1:8888/api/sessions?token=migi`).stdout;
   SESSIONS = JSON.parse(SESSIONS);
-  res.redirect('/');
+  res.redirect(req.get('referer'));
+});
+
+app.get('/API', (req, res) => {
+  getImageFiles();
+  getAI();
+  res.render('function.ejs', {title: 'API', kernels: KERNELS, sessions: SESSIONS, files: DIRCONTENTS, trainProgress:TRAINPROGRESS, notebooks: NOTEBOOKS});
 });
 
 app.post('/create_session', (req, res) => {
@@ -55,10 +65,41 @@ app.post('/create_session', (req, res) => {
                           http://127.0.0.1:8080/session `).stdout;
   test = JSON.parse(test);
   TRAINPROGRESS = test.response;
+  res.redirect('/API');
+});
+
+app.post('/begin_gen', (req, res) =>{
+  const body = {
+    numImages: req.body.numImages,
+  }
+  let test = shell.exec(`curl -d '${JSON.stringify(body)}' \
+                          -H 'Content-Type: application/json' \
+                          http://127.0.0.1:8080/begin_gen `).stdout;
   res.redirect(req.get('referer'));
+});
+
+app.get('/get_notebooks', (req, res) => {
+  shell.cd(NOTEBOOKSPATH)
+  NOTEBOOKS = shell.exec("ls *.ipynb").stdout.trim().split("\n");
+  shell.cd("../")
+  res.render('function.ejs', {title: 'API', kernels: KERNELS, sessions: SESSIONS, files: DIRCONTENTS, trainProgress:TRAINPROGRESS, notebooks: NOTEBOOKS});
+});
+
+app.get('/get_kernels', (req, res) => {
+  KERNELS = shell.exec(`curl -X GET http://127.0.0.1:8888/api/kernels?token=migi`).stdout;
+  KERNELS = JSON.parse(KERNELS);
+  res.redirect(req.get('referer'))
+});
+
+app.get('/create_kernels', (req, res) => {
+  let test = shell.exec(`curl -X POST http://127.0.0.1:8888/api/kernels?token=migi`).stdout;
+  test = JSON.parse(test);
+  console.log(test);
+  res.redirect(req.get('referer'))
 });
 
 app.get('/shell_test', (req, res) => {
   shell.echo("We in term");
   res.status(404).render('404', { title: '404' });
 });
+
--- tensor_flow/load.py
+++ tensor_flow/load.py
@@ -1,29 +1,35 @@
 import tensorflow as tf
 import numpy as np
-import tensorflow_datasets as tfds
 import matplotlib.pyplot as plt
-import pathlib
+from pathlib import Path
 import os
 from sklearn.model_selection import train_test_split
+import sys
 
-def make_noise(n, z):
-    return np.random.normal(0, 1, size=(n,z))
+numImg = int(sys.argv[1])
 
-n = 5
-z = 100
+for index in range(numImg):
+    def make_noise(n, z):
+        return np.random.normal(0, 1, size=(n,z))
 
-path = os.getcwd() + "/OneDrive - sunyit.edu/Capstone/Capstone/tensor_flow"
-Generator = tf.keras.models.load_model(path + '/Generator.h5')
-samples = Generator.predict(make_noise(n,z))
-samples = (samples + 1.0) / 2.0
-plt.figure(figsize=(15,6))
-for i in range(n):
-    plt.subplot(1,n, (i+1))
-    plt.imshow(samples[i].reshape(128,128,3))
-    plt.axis('off')
-plt.savefig(path + "/ProducedSamples/Sample_From_Loaded_Model",
-           bbox_inches='tight',
-           pad_inches = 0.5,
-           transparent = False,
-           dpi =400)
-plt.show()
+    n = 10
+    z = 100
+
+    path = os.getcwd() + "./"
+    Generator = tf.keras.models.load_model('./Generator.h5')
+    samples = Generator.predict(make_noise(n,z))
+    samples = (samples + 1.0) / 2.0
+    plt.figure(figsize=(10,3))
+    for i in range(n):
+        plt.subplot(2,int(n/2), (i+1))
+        plt.imshow(samples[i].reshape(128,128,3))
+        plt.axis('off')
+
+    Path("/Samples").mkdir(parents=True, exist_ok=True)
+
+    plt.savefig(f'./Samples/Sample_From_Loaded_Model_{index}',
+               bbox_inches='tight',
+               pad_inches = 0.2,
+               transparent = False,
+               dpi =400)
+    plt.show()
