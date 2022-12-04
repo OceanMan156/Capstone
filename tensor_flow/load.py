a273d79eadc7b278f1a09ea14dca36185e9b92b4
Miguel Delgado <delgadma@sunypoly.edu>
2022-12-04 01:00:42 -0500

    improved architecture stability

--- tensor_flow/load.py
+++ tensor_flow/load.py
@@ -9,21 +9,21 @@ from sklearn.model_selection import train_test_split
 def make_noise(n, z):
     return np.random.normal(0, 1, size=(n,z))
 
-n = 5
+n = 16
 z = 100
 
 path = os.getcwd() + "/OneDrive - sunyit.edu/Capstone/Capstone/tensor_flow"
 Generator = tf.keras.models.load_model(path + '/Generator.h5')
 samples = Generator.predict(make_noise(n,z))
 samples = (samples + 1.0) / 2.0
-plt.figure(figsize=(15,6))
+plt.figure(figsize=(4,4))
 for i in range(n):
-    plt.subplot(1,n, (i+1))
-    plt.imshow(samples[i].reshape(128,128,3))
+    plt.subplot(4,4, (i+1))
+    plt.imshow(samples[i].reshape(64,64,3))
     plt.axis('off')
 plt.savefig(path + "/ProducedSamples/Sample_From_Loaded_Model",
            bbox_inches='tight',
-           pad_inches = 0.5,
+           pad_inches = 0,
            transparent = False,
            dpi =400)
 plt.show()
